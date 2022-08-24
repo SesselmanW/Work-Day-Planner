9689d5f78c4fb05dcff6a0e6ccf0f35cd8f419f4
William Sesselman <will@Williams-MBP.fios-router.home>
2022-08-24 18:29:37 -0400

    taking a break

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Sets the current date at the top of the page
 var currentDay = moment().format('LL')
 $("#currentDay").text(currentDay)
 console.log(currentDay)
+
+//Set the Current Hour (To be used to color code the planner)
+var currentHour = moment().format("HH")
+console.log(currentHour)
+
+// $(".time-block").each(function(){
+//     var timeDiv = $(this).attr("id");
+//     console.log()
+//     })
+// )
