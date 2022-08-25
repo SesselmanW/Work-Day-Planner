7fe67f692b6263cc148c9a4e7830d1ce47fcaa21
William Sesselman <will@Williams-MBP.fios-router.home>
2022-08-25 15:57:41 -0400

    got color working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 // Sets the current date at the top of the page
-var currentDay = moment().format('LL')
-$("#currentDay").text(currentDay)
-console.log(currentDay)
+
+var currentDay = moment().format('LL');
+$("#currentDay").text(currentDay);
+console.log(currentDay);
 
 //Set the Current Hour (To be used to color code the planner)
-var currentHour = moment().format("HH")
-console.log(currentHour)
+var currentHour = moment().format("HH");
+console.log(currentHour);
 
-// $(".time-block").each(function(){
-//     var timeDiv = $(this).attr("id");
-//     console.log()
-//     })
-// )
+$(".time-block").each(function(){
+    var timeDiv = $(this).attr("id");
+    console.log(timeDiv);
+
+    if (currentHour == timeDiv) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    } else if (currentHour < timeDiv) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past")
+    }
+})
+
+
