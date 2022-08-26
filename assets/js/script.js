7a83dad3466c8a098b6e256ee1a265ce0f1fcb02
William Sesselman <will@Williams-MBP.fios-router.home>
2022-08-26 09:50:33 -0400

    almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,25 @@ var currentDay = moment().format('LL');
 $("#currentDay").text(currentDay);
 console.log(currentDay);
 
-//Set the Current Hour (To be used to color code the planner)
+// Set the Current Hour (To be used to color code the planner)
 var currentHour = moment().format("HH");
 console.log(currentHour);
-
+// Compares the Id in the HTML with the Current Hour Variable to determine colors
 $(".time-block").each(function(){
     var timeDiv = $(this).attr("id");
     console.log(timeDiv);
 
+// Current Hours = RED
     if (currentHour == timeDiv) {
         $(this).removeClass("future");
         $(this).removeClass("past");
         $(this).addClass("present");
+// Future Hours = GREEN
     } else if (currentHour < timeDiv) {
         $(this).removeClass("past");
         $(this).removeClass("present");
         $(this).addClass("future");
+// Past Hours = GREY
     } else {
         $(this).removeClass("future");
         $(this).removeClass("present");
@@ -27,4 +30,9 @@ $(".time-block").each(function(){
     }
 })
 
-
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+    console.log(localStorage);
+})
